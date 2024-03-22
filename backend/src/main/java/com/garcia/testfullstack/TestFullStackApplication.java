6d6db2ed6e71fb012050950de27e6966ad7715b1
chekodev <chekodeveloper@gmail.com>
2024-03-22 17:58:45 -0600

    📁Initialize Spring Boot Project

---
+++ backend/src/main/java/com/garcia/testfullstack/TestFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.garcia.testfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFullStackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/garcia/testfullstack/TestFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garcia.testfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
